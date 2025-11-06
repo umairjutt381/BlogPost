8117cde381469491c6f40dc1ef1b48a99b67a342
umairjutt381 <ch.umairjutt381@gmail.com>
2025-11-06 12:02:35 +0500

    added comment delete api

--- newapp/urls.py
+++ newapp/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('post/new/', views.post_create, name='post_create'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
+    path("comment/delete/<int:pk>/", views.delete_comments, name="delete_comments"),
 ]
